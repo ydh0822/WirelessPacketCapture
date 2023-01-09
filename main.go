fdafc00c13f78866d0357ca10090462eba57de79
H4uN <dohyun0822@gmail.com>
2023-01-09 16:07:53 +0900

    1init

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/google/gopacket"
 	_ "github.com/google/gopacket/layers"
@@ -33,28 +32,11 @@ func WPC_() {
 	// if err := handle.SetBPFFilter("port 3030"); err != nil {
 	// 	panic(err)
 	// }
-
 	packets := gopacket.NewPacketSource(
 		handle, handle.LinkType()).Packets()
 	for pkt := range packets {
-		// Your analysis here!
-		fmt.Print("\033[H\033[2J")
+		// fmt.Print("\033[H\033[2J")
 		fmt.Println(pkt)
-		fmt.Print("1.")
-		fmt.Println(pkt.Layers())
-		fmt.Print("2.")
-		fmt.Println(pkt.Metadata().AncillaryData...)
-		fmt.Print("3.")
-		fmt.Println(pkt.Data())
-		fmt.Print("4.")
-		fmt.Println(pkt.Dump())
-		fmt.Print("5.")
-		fmt.Println(pkt.Metadata())
-		fmt.Print("6.")
-		fmt.Println(pkt.String())
-		fmt.Print("7.")
-		fmt.Println(pkt.NetworkLayer())
-		fmt.Print("8.")
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 	}
 }
